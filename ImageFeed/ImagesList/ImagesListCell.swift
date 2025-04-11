7bcd048d45f9c0811607e4e25ff817173bf7be40
Andrei Kigai <avkigai@gmail.com>
2025-04-11 15:30:24 +0300

    Добавил ячеек с картинками согласно дизайну

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -2,4 +2,8 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var backgroundImageView: UIImageView!
 }
--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -3,27 +3,46 @@ import UIKit
 final class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
     
+    private let photosName: [String] = Array(0..<20).map { "\($0)" }
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        formatter.locale = Locale(identifier: "ru_RU")
+        return formatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
-//        tableView.register(
-//            ImagesListCell.self,
-//            forCellReuseIdentifier: ImagesListCell.reuseIdentifier
-//        )
         
         tableView.rowHeight = 200
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
 }
 
 extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return 0
+        }
+        
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let imageWidth = image.size.width
+        let scale = imageViewWidth / imageWidth
+        return image.size.height * scale + imageInsets.top + imageInsets.bottom
+    }
 }
 
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return photosName.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -34,13 +53,28 @@ extension ImagesListViewController: UITableViewDataSource {
             return UITableViewCell()
         }
         
-        configCell(for: imageListCell)
+        configCell(for: imageListCell, with: indexPath)
         
         return imageListCell
     }
     
-    private func configCell(for cell: ImagesListCell) {
-        cell.layer.cornerRadius = 16
-        cell.layer.masksToBounds = true
+    private func configCell(for cell: ImagesListCell, with index: IndexPath) {
+        let imageName = photosName[index.row]
+        guard let image = UIImage(named: imageName) else {
+            return
+        }
+        cell.backgroundImageView.image = image
+        cell.backgroundImageView.layer.cornerRadius = 16
+        cell.backgroundImageView.layer.masksToBounds = true
+        cell.backgroundImageView.contentMode = .scaleAspectFit
+        
+        cell.dateLabel.text = dateFormatter.string(from: Date())
+        
+        let likeImage = if index.row % 2 == 0 {
+            UIImage(named: "LikeActive")
+        } else {
+            UIImage(named: "LikeInactive")
+        }
+        cell.likeButton.setImage(likeImage, for: .normal)
     }
 }
