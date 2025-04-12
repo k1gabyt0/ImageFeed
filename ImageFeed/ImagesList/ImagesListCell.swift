75f0782b56c57edf060a46e86c1e46780e5503c8
Andrei Kigai <avkigai@gmail.com>
2025-04-12 12:18:38 +0300

    Format code using swift-format

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -2,7 +2,7 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
-    
+
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var likeButton: UIButton!
     @IBOutlet var backgroundImageView: UIImageView!
