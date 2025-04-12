48518c07118f3b6a7caaad3bba7ec1ca9709cc4f
Andrei Kigai <avkigai@gmail.com>
2025-04-12 14:46:31 +0300

    Сверстал страницу профиля

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var nickNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    @IBOutlet private var logoutButton: UIButton!
+
+    @IBAction private func onLogoutButtonTapped(_ sender: Any) {
+    }
+}
