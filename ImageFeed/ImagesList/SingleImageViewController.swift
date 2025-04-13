503caace89a02d150c2a8c201a24856f847103ed
Andrei Kigai <avkigai@gmail.com>
2025-04-13 13:36:33 +0300

    Сделал переход модальным

--- ImageFeed/ImagesList/SingleImageViewController.swift
+++ ImageFeed/ImagesList/SingleImageViewController.swift
@@ -2,11 +2,15 @@ import UIKit
 
 final class SingleImageViewController: UIViewController {
     var image: UIImage?
-    
+
     @IBOutlet private var imageView: UIImageView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+    }
+
+    @IBAction func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true)
     }
 }
