00336864c0097d66fed8f23817c19d949deb0de1
Andrei Kigai <avkigai@gmail.com>
2025-04-16 15:30:42 +0300

    [Sprint 09] TabBar, Scrollview и верстка кодом (#2)

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -5,32 +5,66 @@ private enum Constants {
     static let likeInactiveImageName = "LikeInactive"
 }
 
-
 final class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
-    
+
     private let imageNames: [String] = (0..<20).map(String.init)
-    
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         tableView.delegate = self
         tableView.dataSource = self
-        
-        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+
+        tableView.contentInset = UIEdgeInsets(
+            top: 12,
+            left: 0,
+            bottom: 12,
+            right: 0
+        )
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showSingleImageSegueIdentifier {
+            guard
+                let viewController = segue.destination
+                    as? SingleImageViewController,
+                let indexPath = sender as? IndexPath
+            else {
+                return
+            }
+
+            let image = UIImage(named: imageNames[indexPath.row])
+            viewController.image = image
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
     }
 }
 
 extension ImagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    func tableView(
+        _ tableView: UITableView,
+        didSelectRowAt indexPath: IndexPath
+    ) {
+        performSegue(
+            withIdentifier: showSingleImageSegueIdentifier,
+            sender: indexPath
+        )
+    }
+
+    func tableView(
+        _ tableView: UITableView,
+        heightForRowAt indexPath: IndexPath
+    ) -> CGFloat {
         guard let image = UIImage(named: imageNames[indexPath.row]) else {
             return 0
         }
-        
+
         let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
-        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let imageViewWidth =
+            tableView.bounds.width - imageInsets.left - imageInsets.right
         let imageWidth = image.size.width
         let scale = imageViewWidth / imageWidth
         return image.size.height * scale + imageInsets.top + imageInsets.bottom
@@ -38,40 +72,48 @@ extension ImagesListViewController: UITableViewDelegate {
 }
 
 extension ImagesListViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)
+        -> Int
+    {
         imageNames.count
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)
+        -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: ImagesListCell.reuseIdentifier,
+            for: indexPath
+        )
         guard let imageListCell = cell as? ImagesListCell else {
             // TODO Тут лучше логировать ошибку
             return UITableViewCell()
         }
-        
+
         configCell(for: imageListCell, with: indexPath)
-        
+
         return imageListCell
     }
-    
+
     private func configCell(for cell: ImagesListCell, with index: IndexPath) {
         let imageName = imageNames[index.row]
         guard let image = UIImage(named: imageName) else {
             return
         }
-        
+
         cell.backgroundImageView.image = image
         cell.backgroundImageView.layer.cornerRadius = 16
         cell.backgroundImageView.layer.masksToBounds = true
         cell.backgroundImageView.contentMode = .scaleAspectFit
-        
+
         cell.dateLabel.text = DateFormatter.russianDate.string(from: Date())
-        
-        let likeImage = if index.row % 2 == 0 {
-            UIImage(named: Constants.likeActiveImageName)
-        } else {
-            UIImage(named: Constants.likeInactiveImageName)
-        }
+
+        let likeImage =
+            if index.row % 2 == 0 {
+                UIImage(named: Constants.likeActiveImageName)
+            } else {
+                UIImage(named: Constants.likeInactiveImageName)
+            }
         cell.likeButton.setImage(likeImage, for: .normal)
     }
 }
---
+++ ImageFeed/ImagesList/SingleImageViewController.swift
@@ -0,0 +1,114 @@
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    var image: UIImage?
+
+    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet private var scrollView: UIScrollView!
+
+    private let minScale = 0.1
+    private let maxScale = 1.25
+    private var fullScreenScale: CGFloat = 1.0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        scrollView.minimumZoomScale = minScale
+        scrollView.maximumZoomScale = maxScale
+
+        guard let image = image else { return }
+
+        imageView.image = image
+        imageView.frame.size = image.size
+        view.layoutIfNeeded()
+
+        fullScreenScale = calculateScaleFor(image: image)
+        rescaleAndCenterImageInScrollView()
+    }
+
+    @IBAction func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true)
+    }
+
+    @IBAction func didTapShareButton(_ sender: Any) {
+        guard let image = image else { return }
+
+        let shareView = UIActivityViewController(
+            activityItems: [image],
+            applicationActivities: nil
+        )
+        present(shareView, animated: true, completion: nil)
+    }
+
+    private func rescaleAndCenterImageInScrollView(animated: Bool = false) {
+        scrollView.setZoomScale(fullScreenScale, animated: animated)
+        scrollView.layoutIfNeeded()
+
+        let visibleRectSize = scrollView.bounds.size
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.contentOffset.x = x
+        scrollView.contentOffset.y = y
+    }
+
+    private func calculateScaleFor(image: UIImage) -> CGFloat {
+        view.layoutIfNeeded()
+
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+
+        let imageSize = image.size
+        guard imageSize.width > 0, imageSize.height > 0 else { return 1 }
+
+        let visibleRectSize = scrollView.bounds.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+
+        return min(
+            maxZoomScale,
+            max(minZoomScale, max(hScale, vScale))
+        )
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
+    }
+
+    // Центрируем картинку во время зума с помощью contentInset
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let newContentSize = scrollView.contentSize
+        let visibleRectSize = scrollView.bounds.size
+
+        var hInset: CGFloat = 0
+        if newContentSize.width < visibleRectSize.width {
+            hInset = (visibleRectSize.width - newContentSize.width) / 2
+        }
+        var vInset: CGFloat = 0
+        if newContentSize.height < visibleRectSize.height {
+            vInset = (visibleRectSize.height - newContentSize.height) / 2
+        }
+
+        scrollView.contentInset = UIEdgeInsets(
+            top: vInset,
+            left: hInset,
+            bottom: vInset,
+            right: hInset
+        )
+    }
+
+    // Откатываемся по красоте на дефолтный масштаб если отдалили картинку далеко
+    func scrollViewDidEndZooming(
+        _ scrollView: UIScrollView,
+        with view: UIView?,
+        atScale scale: CGFloat
+    ) {
+        if scale >= fullScreenScale {
+            return
+        }
+
+        rescaleAndCenterImageInScrollView(animated: true)
+    }
+}
